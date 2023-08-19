b5f6adedf243a2ce72bab492be24f01832e0ebca
John R Milinovich <jrmilinovich@gmail.com>
2023-08-19 09:55:16 -0700

    initialize repo

---
+++ constants.py
@@ -0,0 +1,17 @@
+CONCEPTS = [
+    "the golden gate bridge",
+    "a ferrari driving on an alpine road",
+    "a running back scoring a touchdown",
+    "children picking fruit on an orchard",
+    "a modern mansion in the middle of a lush forest",
+    "an old man sitting on the subway reading a newspaper"
+]
+
+STYLES = {
+    "boho_watercolor_illustration": "[concept], in the style of soft romantic simple boho scenes, watercolor illustration few details, children's book illustration, two dimensional",
+    "dreamy_realistic_landscape": "an oil painting of [concept], in the style of photobashing, detailed architectural drawings, luminous watercolors, uhd image, engineering/construction and design, multilayered dimensions",
+    "pen_detailed_illustration": "pencil & ink drawing of [concept], black and white, in the style of whimsical children's book illustrator, rough-edged 2d animation, hideyuki kikuchi, panoramic scale"
+}
+
+MODEL_URL = "stability-ai/sdxl:a00d0b7dcbb9c3fbb34ba87d2d5b46c56969c84a628bf778a7fdaec30b1b99c5"
+NUM_OUTPUTS = 2
---
+++ main.py
@@ -0,0 +1,48 @@
+import keys
+import constants
+import os
+import json
+import replicate
+
+os.environ["REPLICATE_API_TOKEN"] = keys.REPLICATE_API_TOKEN
+CONCEPTS = constants.CONCEPTS
+STYLES = constants.STYLES
+MODEL_URL = constants.MODEL_URL
+NUM_OUTPUTS = constants.NUM_OUTPUTS
+
+class ImageGenerator:
+    def __init__(self, concepts, styles, model_url, num_outputs):
+        self.concepts = concepts
+        self.styles = styles
+        self.model_url = model_url
+        self.num_outputs = num_outputs
+
+    def generate_image(self, concept, style):
+        prompt = style.replace("[concept]", concept)
+        images = replicate.run(
+            self.model_url,
+            input={"prompt": prompt, "num_outputs": self.num_outputs})
+        return {
+            "CONCEPT": concept,
+            "URLs": images
+        }
+
+    def generate_images_for_style(self, style):
+        results = {
+            "BASE_STYLE": style,
+            "CONCEPTS": []
+        }
+        for concept in self.concepts:
+            result = self.generate_image(concept, style)
+            results["CONCEPTS"].append(result)
+        return results
+
+    def generate_and_save_all_images(self):
+        for style_name, style in self.styles.items():
+            result_json = self.generate_images_for_style(style)
+            filename = f'{style_name}.json'
+            with open(filename, 'w') as file:
+                json.dump(result_json, file, indent=4)
+
+image_generator = ImageGenerator(CONCEPTS, STYLES, MODEL_URL, NUM_OUTPUTS)
+image_generator.generate_and_save_all_images()
