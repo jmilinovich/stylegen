773cbc26905ad6e9b4bc43a8ced8bb1741ef9c31
John R Milinovich <jrmilinovich@gmail.com>
2023-08-19 17:22:31 -0700

    making runner more flexible

--- runner.py
+++ runner.py
@@ -1,9 +1,13 @@
 import subprocess
 import webbrowser
+import sys
+
+# Get the current Python executable
+python_executable = sys.executable
 
 # Running MAIN.PY
 print("Running MAIN.PY...")
-result_main = subprocess.run(['python', 'MAIN.PY'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result_main = subprocess.run([python_executable, 'MAIN.PY'], stderr=subprocess.PIPE)
 
 if result_main.returncode == 0:
     print("MAIN.PY executed successfully!")
@@ -14,7 +18,7 @@ else:
 
 # Running PAGEMAKER.PY
 print("Running PAGEMAKER.PY...")
-result_pagemaker = subprocess.run(['python', 'PAGEMAKER.PY'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result_pagemaker = subprocess.run([python_executable, 'PAGEMAKER.PY'], stderr=subprocess.PIPE)
 
 if result_pagemaker.returncode == 0:
     print("PAGEMAKER.PY executed successfully!")
@@ -28,4 +32,4 @@ print("Both scripts executed successfully!")
 # Open the newly created index.html file in the default web browser
 index_file_path = 'pages/index.html'
 print(f"Opening {index_file_path} in the default web browser...")
-webbrowser.open(index_file_path, new=2)
+webbrowser.open(index_file_path)
